3473e4800b20e5c8b7b05fec48d04bbc98b2da5e
Pedro <pedro.drummond33@gmail.com>
2022-08-16 20:05:09 -0300

    modificações sem importancia

--- jogo.c
+++ jogo.c
@@ -63,11 +63,10 @@ void validar(char jogo[][3]){
 }
 
 int vitoria(char jogo[][3]){
-    /*
-    if((T[0][0] == T[1][1])&&(T[1][1] == T[2][2]) || (T[0][2] == T[1][1])&&(T[1][1] == T[2][0]) ||((T[0][0] == T[0][1])&&(T[0][1] == T[0][2])) || ((T[1][0] == T[1][1])&&(T[1][1] == T[1][2])) || ((T[2][0] == T[2][1])&&(T[2][1] == T[2][2])) || ((T[0][0] == T[1][0])&&(T[1][0]==T[2][0])) || ((T[0][1] == T[1][1])&&(T[1][1]==T[2][1])) || ((T[0][2] == T[1][2])&&(T[1][2]==T[2][2]))){
-        printf("Jogo vencido\n");
-    }
-    */
+    // -2 = velha
+    // -1 = muitas vitorias
+    // 1 = vitoria
+
     char temp;
     int vrow = 0, vcol = 0, vdiag1 = 0, vdiag2 = 0;
     int vitoria = -2;//velha
@@ -79,7 +78,7 @@ int vitoria(char jogo[][3]){
     }
     //verificar mais de uma vitoria na linha
     if(vrow > 1)
-        return -1;  //uma vitoria
+        return -1;  //mais de uma vitoria
     //Game over - col
     for (int j = 0; j < 3; j++){
         if (jogo[0][j] == jogo[1][j] && jogo[1][j] == jogo[2][j] && jogo[0][j] != 'V'){
@@ -88,7 +87,7 @@ int vitoria(char jogo[][3]){
     }
     //verificar mais de uma vitoria na coluna
     if(vcol > 1)
-        return -1;  //uma vitoria
+        return -1;  //mais de uma vitoria
     //game over - diagonal
     for (int i = 0; i < 3; i++){
         if (jogo[i][i] == jogo[i][i+1] && jogo[i][i+1] == jogo[i][i+2] && jogo[i][i] != 'V'){
