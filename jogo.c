ac5d012b51150a581f5bb900dede7dde93d30972
Ces4rGabriel <cesargabrieldeapulo@gmail.com>
2022-08-16 20:37:51 -0300

    Criando Variáveis

--- jogo.c
+++ jogo.c
@@ -21,6 +21,8 @@ void imprimir(char jogo[][3]){
 }
 int validar(char jogo[][3]){
 
+    int QuantX, int QuantO;
+
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++){
             if((jogo[i][j] != 'X') || (jogo[i][j] != 'O') || (jogo[i][j] != 'V'))
